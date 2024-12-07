8adfb05f55337bf6ad3f1b9e22ef1fbfc7d0b299
Agnieszka Grabowska <aneszka0301@gmail.com>
2024-12-07 15:21:44 +0100

    add day 7

---
+++ 2024/Day 7/partTwo.ts
@@ -0,0 +1,46 @@
+import input from "../../read-input.ts";
+
+const operators = ["+", "*", "||"];
+const numOfOperators = operators.length;
+
+const equations: number[][] = input
+  .split("\n")
+  .map((equation) => equation.split(/: | /g).map((n) => Number(n)));
+
+let totalCalibrationResult = 0;
+
+for (const equation of equations) {
+  const testValue = equation[0];
+  const numbers = equation.slice(1);
+
+  const totalCombinations = numOfOperators ** (numbers.length - 1);
+
+  const results = Array(totalCombinations).fill(numbers[0]);
+
+  for (let i = 0; i < totalCombinations; i++) {
+    const combinations = [];
+    let temp = i;
+
+    for (let j = 0; j < numbers.length - 1; j++) {
+      combinations.push(operators[temp % numOfOperators]);
+      temp = Math.floor(temp / numOfOperators);
+    }
+
+    for (let n = 1; n < numbers.length; n++) {
+      if (combinations[n - 1] === "*") {
+        results[i] *= numbers[n];
+      }
+      if (combinations[n - 1] === "||") {
+        results[i] = Number(`${results[i]}${numbers[n]}`);
+      }
+      if (combinations[n - 1] === "+") {
+        results[i] += numbers[n];
+      }
+    }
+    if (results[i] === testValue) break;
+  }
+
+  if (results.includes(testValue)) totalCalibrationResult += testValue;
+}
+
+console.log("Total calibration result: " + totalCalibrationResult);
